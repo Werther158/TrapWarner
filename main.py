36a190ae8dabc42d39a4a59710db2835025f32b0
Werther158 <werther.berselli@gmail.com>
2020-02-27 00:18:02 +0100

    Corretto bug di end_of_days, terminato parsing con province, creata notifica del giorno, da mandare via mail. C'è un bug sulle province, l'ultima è MO, ma penso sia un problema della libreria

--- main.py
+++ main.py
@@ -1,81 +1,138 @@
 import pathlib
 import wget
 import os
-import pdfminer   #ho usato pdfminer.six che è il fork attivo mantenuto dalla comunità
+import pdfminer
 from pdfminer import high_level
 import re
+from datetime import date
 
-i = 0
-merda = 0
-casella = []
-province = ["BO", "FE", "FC", "MO", "PR", "PC", "RA", "RE", "RN"]
-start_of_days = []
-end_of_days = []
-righe_province =[]
-url = "https://www.poliziadistato.it/statics/09/emilia.pdf"    #piccolo problema sembra che l'url cambi ogni settimana e per adesso lìho lasciato hardcoded
-download_location = str(pathlib.Path().absolute()) + "\\"+"emilia.pdf"
-print("The download location is " + download_location)
-if os.path.exists(download_location):
-    os.remove("emilia.pdf")                                   #così magari ci evitiamo qualche file inutile, non lo so magari se poi lo mettiamo alla fine per fare della pulizia ma così se crashiamo a metà siamo tranquilli
-    print("removed old file")
-wget.download(url,download_location)
-print("new file downloaded")
-print("now parsing pdf file")                                #questo dovrebbe essere il modo più brutale di estrarre il testo dal pdf
-text = pdfminer.high_level.extract_text(download_location)
-print("pdf parsed")
-#print(repr(text))
-cells = text.split("\n")
-print("modified text =>")
-for elements in cells:
-    # ok il check a seguire sarebbe da mettere un po' a posto era partito tranquillo ma  degradato velocemente
-    if elements == "" or elements == "/" or elements == " " or elements == "Emilia Romagna" or elements=="Emilia Romagna" or elements=="Fonte: Polizia di Stato – Servizio Polizia Stradale " or elements == "Validità  da" or elements == "a" or elements == "Provincia" or elements == "Giorno" or elements == "Tratto stradale" or elements =="Strada Statale" or elements =="Strada Provinciale" or elements=="Strada Comunale" or elements=="Autostrada":
-        merda+=1
-    else:
-        casella.append(elements)                            #un po' di ripulisti generale
-        i+=1
 
-i=0
-for a in casella:
-    print(a + "   indx: " +  str(i))
-    x = re.search("([0-9]{2}\/[0-9]{2}\/[0-9]{4})", a)
-    if (x):
-        start_of_days.append(i)  # lista degli indici di riga delle caselle contenti il giorno del controllo
-        print("^ Its a day")
-    for b in province:
-        if a == b:
-            righe_province.append(i)  # lista degli indici di riga delle caselle conteneti le provincie delle strade dei controlli
-            print("^ Its a provincia")
-    i+=1
+url = "https://www.poliziadistato.it/statics/09/emilia.pdf"
+download_location = str(pathlib.Path().absolute()) + "/"+"emilia.pdf"
 
-for days in start_of_days:
-    print("start_of_days")
-    print(days)
+# Lista dei dati da inviare per email
+data = []
+# Indici dell'inizio dei giorni in data
+idx_date_data = []
 
-for prov in righe_province:
-    print("righe_provincie")
-    print(prov)
+def pdf_fetch():
+    """
+    Pdf fetch
+    :return: none
+    """
+    print("The download location is " + download_location)
+    if os.path.exists(download_location):
+        os.remove("emilia.pdf")
+        print("removed old file")
+    wget.download(url, download_location)
+    print("new file downloaded")
 
-#qui dovremmo riconoscere la fine di ogni giornata ma non funziona benissimo è da mettere a posto sembra che prenda qualcosa di troppo
-for idx, days in enumerate(start_of_days):
-    e=days+1
-    k=0
-    while k==0:
+
+def pdf_parser():
+    """
+    Pdf parser
+    :return: none
+    """
+    i = 0
+    skip = 0
+    casella = []
+    province = ["BO", "FE", "FC", "MO", "PR", "PC", "RA", "RE", "RN"]
+    start_of_days = []
+    end_of_days = []
+    righe_province = []
+
+    print("now parsing pdf file")
+    text = pdfminer.high_level.extract_text(download_location)
+    print("pdf parsed")
+    cells = text.split("\n")
+    print("modified text =>")
+    for elements in cells:
+        if elements == "" or elements == "/" or elements == " " or elements == "Emilia Romagna" \
+                or elements == "Fonte: Polizia di Stato – Servizio Polizia Stradale " \
+                or elements == "Validità  da" or elements == "a" or elements == "Provincia" or elements == "Giorno" \
+                or elements == "Tratto stradale" or elements == "Strada Statale" or elements == "Strada Provinciale" \
+                or elements == "Strada Comunale" or elements == "Autostrada":
+            skip += 1
+        else:
+            casella.append(elements)
+            i += 1
+
+    i = 0
+    for a in casella:
+        print(a + "   index: " + str(i))
+        x = re.search(r"([0-9]{2}/[0-9]{2}/[0-9]{4})", a)
+        if x:
+            # lista degli indici di riga delle caselle contenenti il giorno del controllo
+            start_of_days.append(i)
+            print("^ Its a day")
         for b in province:
-            if casella[e] == b:
-                k=1
-        x = re.search("([0-9]{2}\/[0-9]{2}\/[0-9]{4})", casella[e])
-        if (x):
-            k = 1
-        e+=1
-    end_of_days.append(e)
+            if a == b:
+                # lista degli indici di riga delle caselle contenenti le province delle strade dei controlli
+                righe_province.append(i)
+                print("^ Its a provincia")
+        i += 1
 
-for days in end_of_days:
-    print ("end of days" + str(days))
+    for days in start_of_days:
+        print("start_of_days")
+        print(days)
 
-#qui stampiamo ogni singolo giorno ma ci sono da aggiungere le provincie che vanno caricate separatamente perchè le screpa alla fine di ogni pagina
-for idx, days in enumerate(start_of_days):
-    #print(idx, days)
-    print("Giornata n°" + str(idx+1))
-    if idx < 7:
-        for idx2 in range(days, end_of_days[idx]):
-            print(casella[idx2])
+    for prov in righe_province:
+        print("righe_province")
+        print(prov)
+
+    for idx, days in enumerate(start_of_days):
+        e = days+1
+
+        while True:
+            if casella[e] in province or re.search(r"([0-9]{2}/[0-9]{2}/[0-9]{4})", casella[e]):
+                break
+            e += 1
+        end_of_days.append(e)
+
+    for days in end_of_days:
+        print("end of days" + str(days))
+
+    indice_provincia = 0
+    for idx, days in enumerate(start_of_days):
+        print("Giornata n°" + str(idx+1))
+        if idx < 7:
+            for idx2 in range(days, end_of_days[idx]):
+                if casella[idx2] not in province:
+                    if not re.search(r"([0-9]{2}/[0-9]{2}/[0-9]{4})", casella[idx2]):
+                        data.append(casella[idx2] + " " + casella[righe_province[indice_provincia]])
+                        indice_provincia += 1
+                    else:
+                        idx_date_data.append(len(data))
+                        data.append(casella[idx2])
+                    print(casella[idx2])
+
+
+def send_alert():
+    """
+        Send an email alert
+        :return: none
+        """
+    print("---IT'S ALERT TIME---")
+    idx_controllo = -1
+    for indice in idx_date_data:
+        if data[indice] == date.today().strftime("%d/%m/%Y"):
+            idx_controllo = indice
+
+    if idx_controllo == -1: # Non abbiamo dati per la giornata di oggi
+        return
+
+    # Stampa i controlli di oggi (da inviare via mail)
+    while True:
+        print(data[idx_controllo])
+        idx_controllo += 1
+        if idx_controllo == len(data) or re.search(r"([0-9]{2}/[0-9]{2}/[0-9]{4})", data[idx_controllo]):
+            break
+
+
+"""""""""""
+MAIN BLOCK
+"""""""""""
+
+pdf_fetch()
+pdf_parser()
+send_alert()
--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # unset irrelevant variables
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/werther/Download/TrapWarner/venv"
+set -gx VIRTUAL_ENV "/home/werther/Downloads/TrapWarner/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
